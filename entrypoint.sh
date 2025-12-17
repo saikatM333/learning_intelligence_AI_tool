1e746c74d18434bfe8805ba8e09f0d79b0268eff
saikatM333 <138110246+saikatM333@users.noreply.github.com>
2025-12-17 23:25:41 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,4 +17,4 @@ pytest
 echo "✅ Tests passed successfully"
 
 echo "🌐 Starting FastAPI server..."
-python -m uvicorn app:app --host 0.0.0.0 --port 8000
+python -m uvicorn app:app --host 0.0.0.0 --port $PORT
